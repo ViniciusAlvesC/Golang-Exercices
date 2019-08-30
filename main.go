bb31c254bfb0ac813ab77e413a3200c3b7cd6f37
Vinicius Alves Coutinho <vinicius.alves@sempreceub.com>
2019-08-30 12:06:40 +0000

    Logando mensagem de servidor no ar!

--- main.go
+++ main.go
@@ -2,24 +2,27 @@ package main
 
 
 import (
+    "log"
     "net/http"
     "time"
     "fmt")
 
 func main() {
-    StartServer()
+    go StartServer()
+    time.Sleep(3 * time.Second) //Solução provisória
+    log.Println("[INFO] Servidor no ar!")
 
 }
 
 func getINFO(w http.ResponseWriter, r * http.Request) {
-    qurl: = r.URL.String()
+    qurl := r.URL.String()
     fmt.Fprintf(w, r.Method + " " + qurl)
 }
 
 func StartServer() {
-    duration, _: = time.ParseDuration("1000ns")
+    duration, _ := time.ParseDuration("1000ns")
 
-    server: = & http.Server {
+    server := & http.Server {
         Addr: "192.168.0.5:8082",
         IdleTimeout: duration,
         //	Handler    : 
